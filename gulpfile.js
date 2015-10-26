e424cd6c23d4b6c2c864960187d9b5cecf4bd6b9
ScottPolhemus <scott.polhemus@barrelny.com>
2015-10-25 20:46:44 -0400

    Start fresh

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+
+require('./tasks/sass');
+
+gulp.task('default', ['sass'], function() {
+  livereload.listen();
+
+  gulp.watch(['./**/*.scss'], ['sass']);
+
+  gulp.watch([
+    './**/*.html',
+    './styles/*.css'
+  ], function(event) {
+    livereload.changed(event.path);
+  });
+});
+
+gulp.task('build', ['sass']);
---
+++ tasks/sass.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('sass', function() {
+  return gulp.src('./styles/src/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./styles'));
+});
