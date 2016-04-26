fb61c3647cdd669be8842db4e5b692b905140001
Scott Polhemus <scott.polhemus@barrelny.com>
2016-04-26 15:37:33 -0400

    Add connect server to dev task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
+var connect = require('gulp-connect');
 
 require('./tasks/less');
 
-gulp.task('default', ['less'], function() {
-  livereload.listen();
+gulp.task('dev', ['less'], function() {
+	livereload.listen();
+
+	connect.server({
+		livereload: false
+	});
 
   gulp.watch(['**/*.less'], ['less']);
 
@@ -17,3 +22,5 @@ gulp.task('default', ['less'], function() {
 });
 
 gulp.task('build', ['less']);
+
+gulp.task('default', ['dev']);
